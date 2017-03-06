af52123023055a75916a645d0cd08ab202b97486
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:48:53 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\IframeAppWebclient;
 
-class IframeAppWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	public function init() 
 	{
