af22d1d060968255a6a19ade24f09740c1f46fed
nadine-shi <nadinshi@gmail.com>
2023-02-03 10:44:43 +0300

    fixed BUG - app is open in new tab (instead of iframe) if "Auth token sending mode"="Via POST request"

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -4,14 +4,14 @@ var
 	_ = require('underscore'),
 	ko = require('knockout'),
 	$ = require('jquery'),
-	
+
 	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-	
+
 	App = require('%PathToCoreWebclientModule%/js/App.js'),
 	Screens = require('%PathToCoreWebclientModule%/js/Screens.js'),
-	
+
 	CAbstractScreenView = require('%PathToCoreWebclientModule%/js/views/CAbstractScreenView.js'),
-	
+
 	Settings = require('modules/%ModuleName%/js/Settings.js')
 ;
 
@@ -23,32 +23,42 @@ var
 function CMainView()
 {
 	CAbstractScreenView.call(this, '%ModuleName%');
-	
+
 	/**
 	 * Text for displaying in browser title.
 	 */
 	this.browserTitle = ko.observable(TextUtils.i18n('%MODULENAME%/HEADING_BROWSER_TAB'));
-	
+
 	this.sAuthToken = Settings.AuthMode === Enums.IframeAppAuthMode.NoAuthentication ? '' : $.cookie('AuthToken');
 	this.iTokenMode = Settings.TokenMode;
-	
-	var 
+
+	var
 		sFrameUrl = Settings.Url || '',
 		aFrameUrlParts = sFrameUrl.split('?'),
 		aUrlParams = aFrameUrlParts[1] ? aFrameUrlParts[1].split('&') : []
 	;
-	
+
 	if (this.sAuthToken !== '' && Settings.TokenMode === Enums.IframeAppTokenMode.GETRequest)
 	{
 		aUrlParams.push('aurora_auth_token=' + this.sAuthToken);
 	}
-	
+
 	this.sFrameUrl = aFrameUrlParts[0] + (aUrlParams.length > 0 ? '?' + aUrlParams.join('&') : '');
-	
-	this.elForm = ko.observable(null);
-	this.sIframeName = '%ModuleName%' + 'iframe';
-	this.bIframeLoaded = false;
-	
+
+	this.formDom = ko.observable(null);
+	this.iframeDom = ko.observable(null);
+	this.isFormSubmitted = false;
+	ko.computed(() => {
+		if (!this.isFormSubmitted &&
+			this.shown() &&
+			this.iframeDom() && this.iframeDom().length > 0 &&
+			this.formDom() && this.formDom().length > 0
+		) {
+			this.formDom()[0].submit();
+			this.isFormSubmitted = true;
+		}
+	});
+
 	App.broadcastEvent('%ModuleName%::ConstructView::after', {'Name': this.ViewConstructorName, 'View': this});
 }
 
@@ -59,21 +69,15 @@ CMainView.prototype.ViewConstructorName = 'CMainView';
 
 CMainView.prototype.onShow = function ()
 {
-	var 
+	var
 		Routing = require('%PathToCoreWebclientModule%/js/Routing.js'),
 		sAppHash = Settings.AppName !== '' ?  TextUtils.getUrlFriendlyName(Settings.AppName) : Settings.HashModuleName
 	;
-	
+
 	if (Settings.AuthMode === Enums.IframeAppAuthMode.CustomCredentialsSetByUser && !(Settings.Login !== '' && Settings.HasPassword))
 	{
 		Routing.setHash(['settings', sAppHash]);
 		Screens.showError(TextUtils.i18n('%MODULENAME%/ERROR_EMPTY_LOGIN_RASSWORD', {'APPNAME': Settings.AppName}));
-	}
-	
-	if (!this.bIframeLoaded && this.elForm() && this.elForm()[0])
-	{
-		this.elForm().submit();
-		this.bIframeLoaded = true;
 	}
 };
 
