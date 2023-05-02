be25c10db161638f6ecd6b2ee6a68a05decc28cf
nadine-shi <nadinshi@gmail.com>
2023-05-02 15:16:04 +0300

    Switched to Quasar2/VueJs3 (doBeforeRouteLeave method)

--- vue/components/IframeAppAdminSettings.vue
+++ vue/components/IframeAppAdminSettings.vue
@@ -92,7 +92,7 @@ export default {
   },
 
   beforeRouteLeave(to, from, next) {
-    this.doBeforeRouteLeave(to, from, next)
+    this.$root.doBeforeRouteLeave(to, from, next)
   },
 
   computed: {
--- vue/components/IframeAppAdminSettingsPerUser.vue
+++ vue/components/IframeAppAdminSettingsPerUser.vue
@@ -94,7 +94,7 @@ export default {
   },
 
   beforeRouteLeave(to, from, next) {
-    this.doBeforeRouteLeave(to, from, next)
+    this.$root.doBeforeRouteLeave(to, from, next)
   },
 
   methods: {
